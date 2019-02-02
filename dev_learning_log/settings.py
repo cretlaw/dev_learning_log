fd7ebe80c58f4e8cd606fe4eb0b93845d14f83d3
winterMute84 <wcarbajal84@gmail.com>
2019-02-01 19:49:52 -0800

    added dev_learning_logs app to the project and created two tables

--- dev_learning_log/settings.py
+++ dev_learning_log/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    'dev_learning_logs',
 ]
 
 MIDDLEWARE = [
---
+++ dev_learning_logs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DevLearningLogsConfig(AppConfig):
+    name = 'dev_learning_logs'
